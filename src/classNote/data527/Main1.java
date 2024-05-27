01cae50d2ca25429b0ab808ad0c5ee028f303299
qtcyy <qtcyy2005@gmail.com>
2024-05-27 18:30:32 +0800

    init commit

--- src/classNote/data527/Main1.java
+++ src/classNote/data527/Main1.java
@@ -1,15 +1,24 @@
 package classNote.data527;
 
 import javax.swing.JFrame;
+import java.awt.*;
 
-public class Main1 {
-    public static void main(String[] args){
-        JFrame jf=new JFrame("Window");
-        jf.setVisible(true);
+public class Main1 extends JFrame {
+    public Main1(){
+//        JFrame jf=new JFrame("Window");
+        setTitle("Window");
+        setVisible(true);
         //JFrame.EXIT_ON_CLOSE关闭窗口并停止程序
         //JFrame.DO_NOTHING_ON_CLOSE什么都不做
-        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jf.setSize(500,400);
-        jf.setLocation(300,300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500,400);
+        setLocation(300,300);
+        setResizable(false);
+        Container c = getContentPane();
+        c.setBackground(Color.black);
+    }
+    public static void main(String[] args){
+        new Main1();
+
     }
 }
